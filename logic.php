bf039696a89bacaa1c461a7f696a51d2d7c9fe32
kellerflint <kflint0068@gmail.com>
2019-01-06 17:33:24 -0800

    added more examples

--- logic.php
+++ logic.php
@@ -1,9 +1,29 @@
+<?php
+    // Idea is we have members with different levels of access that we 
+    // display different things to. Non-member = 0 <= false, member = 1 <= true
+    $member = 1;
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
 <body>
-    
+    <section class="both">You are a member or a guest</section>
+    <?php 
+        if($member) {
+    ?>
+    <section class="member">You are a member</section>
+    <?php
+        }
+    ?>
+    <?php
+        if(!$member) {
+    ?>
+    <section class="guest">You are a guest</section>
+    <?php
+        }
+    ?>
 </body>
 </html>
